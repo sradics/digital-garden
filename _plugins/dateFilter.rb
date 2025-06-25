91e616cfef775bd0c93f4fe121679a434884c7cc
sradics <radics.sebastian@gmail.com>
2025-06-25 06:04:12 +0200

    Adjusted date filter

--- _plugins/dateFilter.rb
+++ _plugins/dateFilter.rb
@@ -1,16 +1,31 @@
 module Jekyll
   module DateFilter
-    require 'date' 
+    require 'date'
     def date_sort(collection)
       collection.sort_by do |el|
-          if el.data['date_created'] && !el.data['date_created'].empty?
-            cleaned_string = el.data['date_created'].gsub(/(\d+)(st|nd|rd|th)/, '\1')
-            cleaned_string = cleaned_string.gsub(/am|pm/, &:upcase)
-            DateTime.strptime(cleaned_string, "%A, %B %d %Y, %I:%M:%S %p")
-          else
-            # Provide a default value for sorting if 'date_created' is missing
-            DateTime.new(0)  # This will treat missing or empty dates as very old dates
-          end      
+        date_string = el.data['date_created']
+
+        if date_string && !date_string.empty?
+          begin
+            # --- Try parsing the NEW format first ---
+            # Format: "2025-06-24 09:00"
+            DateTime.strptime(date_string, "%Y-%m-%d %H:%M")
+          rescue ArgumentError
+            # --- If it fails, fall back to the OLD format ---
+            # Format: "Friday, May 23rd 2025, 9:59:27 am"
+            begin
+              cleaned_string = date_string.gsub(/(\d+)(st|nd|rd|th)/, '\1')
+              cleaned_string = cleaned_string.gsub(/am|pm/, &:upcase)
+              DateTime.strptime(cleaned_string, "%A, %B %d %Y, %I:%M:%S %p")
+            rescue ArgumentError
+              # If both formats fail, this note will be sorted to the beginning.
+              DateTime.new(0)
+            end
+          end
+        else
+          # Provide a default value for sorting if 'date_created' is missing or empty.
+          DateTime.new(0) # This will treat these as very old dates.
+        end
       end
     end
   end
